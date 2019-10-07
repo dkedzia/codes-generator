bd4a540c26e999b4c74f37bcd81a35ab11531ff4
Dominik Kedzia <dominikkedzia@gmail.com>
2019-10-07 18:36:25 +0200

    View comments added

--- View.class.php
+++ View.class.php
@@ -7,14 +7,17 @@ class View{
         echo "\t-l, --length [NUMBER]\tSetting the length of each code.\n\t\t\t\tDefault is 10.\n";
         echo "\t-a, --amount [NUMBER]\tSetting the amount of generated codes\n\t\t\t\tDefault is 1.\n";
         echo "\t-f, --file [FILE NAME]\tSetting the text file name, to save codes in.\n\t\t\t\tBy default the name is 'codes.txt'.\n\n";
-    }
+    }   // End of printHelp()
+
     public function printCodes($codesArray){
         print_r($codesArray);
-    }
+    }   // End of printCodes()
+
     public function savedToFile($fileName){
         echo "\nOutput saved to file $fileName.\n";
-    }
+    }   // End of savedToFile()
+
     public function tooManyCodes($max){
         echo "\nToo many codes. Max is $max.\n";
-    }
+    }   // End of tooManyCodes()
 };
